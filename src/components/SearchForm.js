4201a8da5d1403966cb94a6875eb031369d8b028
masahiko kawai <masahikokawai81@gmail.com>
2018-07-16 23:48:06 +0900

    Add parent component App.js to receive values

---
+++ src/components/SearchForm.js
@@ -0,0 +1,39 @@
+import React, { Component, PropTypes } from 'react';
+
+class SearchFrom extends Component {
+
+  constructor(props) {
+  	super(props);
+    this.state = {
+      place: '東京タワー',
+    };
+  }
+
+  handlePlaceChange(place) {
+    this.setState({ place });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.place);
+  }
+
+  render() {
+    return (
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <input
+          type="text"
+          value={this.state.place}
+          onChange={e => this.handlePlaceChange(e.target.value)}
+         />
+         <input type="submit" value="検索" />
+      </form>
+    );
+  }
+}
+
+SearchFrom.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchFrom;
