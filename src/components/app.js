0afee2676f93d82a754d83623e77c625530cddec
masahiko kawai <masahikokawai81@gmail.com>
2018-07-16 20:20:07 +0900

    Add input element

--- src/components/app.js
+++ src/components/app.js
@@ -10,20 +10,19 @@ class App extends Component {
     }
   }
 
-  handleMouseOver() {
-    this.setState({ name: 'ABC' })
-  }
-
-  handleMouseOut() {
-    this.setState({ name: 'XYZ' })
+  handleNameChange(e) {
+    console.log(e.target.value);
+    this.setState({ name: e.target.value });
   }
 
   render() {
     return (
-      <div
-        onMouseOver={() => this.handleMouseOver()}
-        onMouseOut={() => this.handleMouseOut()}
-      >
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.handleNameChange(e)}
+        />
         <Greeting name={this.state.name} />
       </div>
     );
