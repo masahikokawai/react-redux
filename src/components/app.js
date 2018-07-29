ec6950a294f15550483dfdbed71d4ff8b45a87cc
masahiko kawai <masahikokawai81@gmail.com>
2018-07-29 09:55:19 +0900

    Add axios to yarn

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
@@ -11,6 +15,11 @@ class App extends Component {
 
   handlePlaceSubmit(place) {
     console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, { params: { address: place } })
+      .then((result) => {
+        console.log(result);
+      });
   }
 
   render() {
@@ -18,6 +27,11 @@ class App extends Component {
       <div>
         <h1>緯度経度検索</h1>
         <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <GeocodeResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
       </div>
     );
   }
