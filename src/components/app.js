362dbf6bbbe3d85c589495ed22ff346b11be4dc8
masahiko kawai <masahikokawai81@gmail.com>
2018-07-29 13:54:46 +0900

    Refactor function of googlemap api

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
 import Map from './Map';
 
-const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
+import { geocode } from '../domain/Geocoder';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location = {
+      location: {
         lat: 35.6585805,
         lng: 139.7454329,
       },
@@ -30,18 +29,14 @@ class App extends Component {
 
   handlePlaceSubmit(place) {
     console.log(place);
-    axios
-      .get(GEOCODE_ENDPOINT, { params: { address: place } })
-      .then((results) => {
-        console.log(results);
-        const data = results.data;
-        const result = data.results[0];
-        switch (data.status) {
+    geocode(place)
+      .then(({ status, address, location }) => {
+        console.log(status);
+        console.log(address);
+        console.log(location);
+        switch (status) {
           case 'OK': {
-            this.setState({
-              address: result.formatted_address,
-              location: result.geometry.location,
-            });
+            this.setState({ address, location });
             break;
           }
           case 'ZERO_RESULTS': {
