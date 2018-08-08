bf40581094948f7de8298896bc65c152d3b594f1
masahiko kawai <masahikokawai81@gmail.com>
2018-08-08 21:17:59 +0900

    Change in side by side with style flex

--- src/components/app.js
+++ src/components/app.js
@@ -76,11 +76,13 @@ class App extends Component {
       <div className="app">
         <h1 className="app-title">緯度経度検索</h1>
         <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
-        <GeocodeResult
-          address={this.state.address}
-          location={this.state.location}
-        />
-        <Map location={this.state.location} />
+        <div className="result-area">
+          <Map location={this.state.location} />
+          <GeocodeResult
+            address={this.state.address}
+            location={this.state.location}
+          />
+        </div>
       </div>
     )
   }
