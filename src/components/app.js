66031b2c16341e438c1fbd66d5f3dcc02ec38c35
masahiko kawai <masahikokawai81@gmail.com>
2018-08-09 09:24:13 +0900

    Add price sort
    yarn add lodash

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import _ from 'lodash';
 
 import SearchForm from './SearchForm'
 import GeocodeResult from './GeocodeResult'
@@ -8,6 +9,8 @@ import HotelsTable from './HotelsTable';
 import { geocode } from '../domain/Geocoder'
 import { searchHotelByLocation } from '../domain/HotelRepository';
 
+const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
+
 class App extends Component {
   constructor (props) {
     super(props)
@@ -16,6 +19,7 @@ class App extends Component {
         lat: 35.6585805,
         lng: 139.7454329,
       },
+      sortKey: 'price',
     };
   }
 
@@ -68,7 +72,7 @@ class App extends Component {
         return []
       })
       .then((hotels) => {
-        this.setState({ hotels })
+        this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) })
       })
       .catch((error) => {
         this.setErrorMessage('通信に失敗しました')
