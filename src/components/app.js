64d7c1c3a532b31f1bce700548c71e33fce37d22
masahiko kawai <masahikokawai81@gmail.com>
2018-08-15 22:47:50 +0900

    Add react-router
    yarn add react-router-dom

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
 
-import SearchPage from './SearchPage'
+import SearchPage from './SearchPage';
+import AboutPage from './AboutPage';
 
 const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
 
 const App = () => (
-  <div className="app">
-    <ul className="left-navi">
-      <li><a href="/">ホテル検索</a></li>
-      <li><a href="/about">About</a></li>
-    </ul>
-    <SearchPage />
-  </div>
+  <Router>
+    <div className="app">
+      <ul className="left-navi">
+        <li><Link to="/">ホテル検索</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={SearchPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App
