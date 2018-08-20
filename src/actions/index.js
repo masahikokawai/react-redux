e86e7057206315ba915994244fda24c12e587c63
masahiko kawai <masahikokawai81@gmail.com>
2018-08-21 07:19:39 +0900

    Change to split Action Creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,53 @@
-export const setPlace = place => ({ type: 'CHANGE_PLACE', place });
+import { geocode } from '../domain/Geocoder'
+// import { searchHotelByLocation } from '../domain/HotelRepository';
+
+export const setPlace = place => dispatch => dispatch({ type: 'CHANGE_PLACE', place });
+
+export const startSearch = () => (dispatch, getState) => {
+  geocode(getState().place)
+    .then(({ status, address, location }) => {
+      switch (status) {
+        case 'OK': {
+          // this.setState({ address, location })
+          // return searchHotelByLocation(location)
+
+          dispatch({ type: 'GEOCODE_FETCHED', address, location })
+          break;
+        }
+        case 'ZERO_RESULTS': {
+          this.setErrorMessage('結果が見つかりませんでした')
+          break
+        }
+        case 'OVER_DAILY_LIMIT': {
+          this.setErrorMessage('OVER_DAILY_LIMIT')
+          break
+        }
+        case 'OVER_QUERY_LIMIT': {
+          this.setErrorMessage('OVER_QUERY_LIMIT')
+          break
+        }
+        case 'REQUEST_DENIED': {
+          this.setErrorMessage('REQUEST_DENIED')
+          break
+        }
+        case 'INVALID_REQUEST': {
+          this.setErrorMessage('INVALID_REQUEST')
+          break
+        }
+        case 'UNKNOWN_ERROR': {
+          this.setErrorMessage('UNKNOWN_ERROR')
+          break
+        }
+        default: {
+          this.setErrorMessage('エラーが発生しました。')
+        }
+      }
+      return []
+    });
+    // .then((hotels) => {
+    //   this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) })
+    // })
+    // .catch((error) => {
+    //   this.setErrorMessage('通信に失敗しました')
+    // })
+};
--- src/containers/SearchForm.js
+++ src/containers/SearchForm.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { geocode } from '../domain/Geocoder'
-// import { searchHotelByLocation } from '../domain/HotelRepository';
-import { setPlace } from '../actions/';
+import { setPlace, startSearch } from '../actions/';
 
 const SearchForm = props => (
   <form
@@ -21,7 +19,7 @@ const SearchForm = props => (
       value={props.place}
       onChange={(e) => {
         e.preventDefault();
-        props.onPlaceChange(e.target.value);
+        props.startSearch();
       }}
     />
     <input className="submit-button" type="submit" value="検索" />
@@ -30,63 +28,12 @@ const SearchForm = props => (
 
 SearchForm.propTypes = {
   place: PropTypes.string.isRequired,
-  onPlaceChange: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+  setPlace: PropTypes.func.isRequired,
+  startSearch: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   place: state.place,
 });
-const mapDispatchToProps = dispatch => ({
-  onPlaceChange: place => dispatch(setPlace(place)),
-  onSubmit: (place) => {
-    geocode(place)
-      .then(({ status, address, location }) => {
-        switch (status) {
-          case 'OK': {
-            // this.setState({ address, location })
-            // return searchHotelByLocation(location)
 
-            dispatch({ type: 'GEOCODE_FETCHED', address, location })
-            break;
-          }
-          case 'ZERO_RESULTS': {
-            this.setErrorMessage('結果が見つかりませんでした')
-            break
-          }
-          case 'OVER_DAILY_LIMIT': {
-            this.setErrorMessage('OVER_DAILY_LIMIT')
-            break
-          }
-          case 'OVER_QUERY_LIMIT': {
-            this.setErrorMessage('OVER_QUERY_LIMIT')
-            break
-          }
-          case 'REQUEST_DENIED': {
-            this.setErrorMessage('REQUEST_DENIED')
-            break
-          }
-          case 'INVALID_REQUEST': {
-            this.setErrorMessage('INVALID_REQUEST')
-            break
-          }
-          case 'UNKNOWN_ERROR': {
-            this.setErrorMessage('UNKNOWN_ERROR')
-            break
-          }
-          default: {
-            this.setErrorMessage('エラーが発生しました。')
-          }
-        }
-        return []
-      });
-      // .then((hotels) => {
-      //   this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) })
-      // })
-      // .catch((error) => {
-      //   this.setErrorMessage('通信に失敗しました')
-      // })
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
+export default connect(mapStateToProps, { setPlace, startSearch })(SearchForm);
